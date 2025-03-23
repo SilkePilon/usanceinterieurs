228bd78a114e784dd4a9e3514a666955e9cd4eff
Silke pilon <silkepilon2009@gmail.com>
2025-03-23 15:14:34 +0100

    refactor: correct section description wording in InstagramSlider component

--- src/components/section/instagramSlider.jsx
+++ src/components/section/instagramSlider.jsx
@@ -78,7 +78,7 @@ const InsagramSlider = ({ text_muted, bg_muted }) => {
         <SectionTitle
           sectionName={"Instagram"}
           sectionTitle={"Een beetje aandacht vragen mag toch?"}
-          sectionDesc={"Onze tijd besteden wij het liefst aan onze opdrachten, huis en gezin maar soms is het ook fijn om met jullie te delen waar wij zo trots mee bezig zijn."}
+          sectionDesc={"Onze tijd besteden wij het liefst aan onze opdrachten, gezin en eigen huis maar soms is het ook fijn om met jullie te delen waar wij zo trots mee bezig zijn."}
           link={"https://www.instagram.com/usanceinterieurs/"}
           button_text={"Volg Ons"}
           bg_muted={bg_muted}
